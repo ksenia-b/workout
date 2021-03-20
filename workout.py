3894475bf6c3d2b48a192467eac5da353c006543
Oksana <ksenia.barshchyk@yandex.ru>
2021-03-20 21:28:03 +0200

    added login, started adding registration

--- workout.py
+++ workout.py
@@ -1,25 +1,41 @@
-from flask import Flask, render_template, request, url_for, session
+from flask import Flask, render_template, request, url_for, session, redirect, g
 from model import db, User
 import bcrypt
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'SecretKey1'
+
 
 @app.route('/')
 def index():
+    if g.user:
+        return 'Currently logged in as ' + g.user
     return render_template('index.html')
 
 
-@app.route('/login', methods=['POST']):
-def login():
-    user = User.query.filter_by(name=request.form['username'].encode('utf-8')).first()
+@app.before_request
+def before_request():
+    g.user = None
+    if 'username' in session:
+        g.user = session['username']
 
+
+@app.route('/login', methods=['POST'])
+def login():
+    user = User.query.filter_by(name=request.form['username']).first()
     if user is not None:
-        if bcrypt.hashpw(request.form['pass'].encode('utf-8'), user.password_hash.encode('utf-8')) == user.password_hash.encode('utf-8'):
+        if request.form['password_hash'] == user.password_hash:
             session['username'] = request.form['username']
-            return url_for('index')
+            return redirect(url_for('index'))
         return 'Password is incorect'
 
     return 'User does not exists!'
+
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        pass
 
 
 if __name__ == '__main__':
